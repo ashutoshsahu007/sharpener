08f49f0d5a23fe608fcdc989180f025c36fc7afa
ashutoshsahu007 <ppaskumar>
2024-04-15 06:55:43 +0530

    Header added

--- FRONTEND/reastaurant/src/App.jsx
+++ FRONTEND/reastaurant/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/Layout/Header";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
