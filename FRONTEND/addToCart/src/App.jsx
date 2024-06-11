bf6e217ae28f555e13231dac36c6520665784c5c
ashutosh sahu <ppaskumar@gmail.com>
2024-06-11 15:51:47 +0530

    done

--- FRONTEND/addToCart/src/App.jsx
+++ FRONTEND/addToCart/src/App.jsx
@@ -1,14 +1,28 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import { useState } from "react";
+import CartProvider from "./store/CartProvider";
 
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(true);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 };
 export default App;
