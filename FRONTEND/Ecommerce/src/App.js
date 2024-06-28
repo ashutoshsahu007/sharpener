a82c2fd805b277897c609f179c3379e847d6d9d8
ashutosh sahu <ppaskumar@gmail.com>
2024-06-28 07:19:00 +0530

    d

--- FRONTEND/Ecommerce/src/App.js
+++ FRONTEND/Ecommerce/src/App.js
@@ -5,22 +5,49 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((MovieData) => {
-          return {
-            id: MovieData.episode_id,
-            title: MovieData.title,
-            openingText: MovieData.opening_crawl,
-            releaseDate: MovieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [error, setError] = useState(null);
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+
+      if (!response.ok) {
+        throw new Error("Something went Wrong!");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((MovieData) => {
+        return {
+          id: MovieData.episode_id,
+          title: MovieData.title,
+          openingText: MovieData.opening_crawl,
+          releaseDate: MovieData.release_date,
+        };
       });
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }
+
+  let content = <p>Found no Movies.</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -28,9 +55,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
