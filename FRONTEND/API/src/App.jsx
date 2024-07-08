c355e2d8378c51725607ff2492a58624fc1e2c22
ashutosh sahu <ppaskumar@gmail.com>
2024-07-08 12:59:35 +0530

    loading state completed

--- FRONTEND/API/src/App.jsx
+++ FRONTEND/API/src/App.jsx
@@ -5,23 +5,38 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
-    setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
+  try {
+    async function fetchMoviesHandler() {
+      setIsLoading(true);
+      setError(null);
+      const response = await fetch("https://swapi.dev/api/films");
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    setIsLoading(false);
+      if (!response.ok) {
+        throw new Error("Somthing went wrong!");
+      }
+
+      const data = await response.json();
+
+    
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+     
+    }
+  } catch (error) {
+    setError(error.message);
   }
+  setIsLoading(false);}
+
 
   return (
     <React.Fragment>
